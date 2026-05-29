4d673e6bc5a0081384033e74112a96da0365518a
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-05-29 15:48:06 -0300

    Add files via upload

---
+++ ej8_tp5_santino_ramirez/src/ej8_tp5_santino_ramirez/Ej8_tp5_santino_ramirez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej8_tp5_santino_ramirez;
+
+/**
+ *
+ * @author ET20 - Alumno
+ */
+public class Ej8_tp5_santino_ramirez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
