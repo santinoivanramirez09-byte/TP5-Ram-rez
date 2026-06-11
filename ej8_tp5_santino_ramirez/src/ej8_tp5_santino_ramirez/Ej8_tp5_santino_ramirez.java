66d275f57211d8fa3a44ffb4a47b4b68a544de7d
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-06-11 11:53:35 -0300

    Add files via upload

--- ej8_tp5_santino_ramirez/src/ej8_tp5_santino_ramirez/Ej8_tp5_santino_ramirez.java
+++ ej8_tp5_santino_ramirez/src/ej8_tp5_santino_ramirez/Ej8_tp5_santino_ramirez.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ej8_tp5_santino_ramirez;
+import java.util.Scanner;
 
-/**
- *
- * @author ET20 - Alumno
- */
 public class Ej8_tp5_santino_ramirez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+   Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese una frase: ");
+        String num3 = scanner.nextLine();
+        String[] num2 = num3.split(" ");
+        String[][] num1 = new String[num2.length][2];
+        for (int i = 0; i < num2.length; i++) {
+            num1[i][0] = num2[i];
+            num1[i][1] = String.valueOf(num2[i].length());
+        }
+        System.out.println("[Palabra]  [Letras]");
+        for (int i = 0; i < num2.length; i++) {
+            System.out.println(num1[i][0] + " - " + num1[i][1]);
+        }
     }
-    
 }
