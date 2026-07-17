e1039373a6ed09bd307cc0b01599fc201097938d
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-07-16 21:49:29 -0300

    Update Ej3_tp5_santino_ramirez.java

--- ej3_tp5_santino_ramirez/src/ej3_tp5_santino_ramirez/Ej3_tp5_santino_ramirez.java
+++ ej3_tp5_santino_ramirez/src/ej3_tp5_santino_ramirez/Ej3_tp5_santino_ramirez.java
@@ -1,7 +1,34 @@
-package ej3_tp5_santino_ramirez;
+import java.util.Scanner;
 
-
-public class Ej3_tp5_santino_ramirez {
-
+public class Main {
     public static void main(String[] args) {
-  
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de filas: ");
+        int filas = teclado.nextInt();
+
+        System.out.print("Ingrese el número de columnas: ");
+        int columnas = teclado.nextInt();
+
+        int[][] matriz = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("Ingrese el elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = teclado.nextInt();
+            }
+        }
+
+        System.out.println("\nMatriz ingresada:");
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        teclado.close();
+    }
+}
