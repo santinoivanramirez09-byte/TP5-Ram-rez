5c3eaf2dd81929f46fa73f02189754db978eac5c
santinoivanramirez09-byte <santinoivanramirez09@gmail.com>
2026-07-16 21:50:07 -0300

    Update Ej9_tp5_santino_ramirez.java

--- ej9_tp5_santino_ramirez/src/ej9_tp5_santino_ramirez/Ej9_tp5_santino_ramirez.java
+++ ej9_tp5_santino_ramirez/src/ej9_tp5_santino_ramirez/Ej9_tp5_santino_ramirez.java
@@ -1,8 +1,25 @@
-
-package ej9_tp5_santino_ramirez;
-
-
-public class Ej9_tp5_santino_ramirez {
-
- 
+public class Main {
     public static void main(String[] args) {
+
+      
+        int[][] matriz = {
+            {5, 3, 0},
+            {8, 2, 0},
+            {10, 7, 0},
+            {4, 9, 0}
+        };
+
+        
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i][2] = matriz[i][0] + matriz[i][1];
+        }
+
+        System.out.println("Matriz:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
